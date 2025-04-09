629993f7ed7f5fae2fe85391f92115121968a949
Adrian Elder <adrian.elder@gmail.com>
2025-04-09 17:45:33 -0400

    test score

--- docs/score/audiosynth.view.js
+++ docs/score/audiosynth.view.js
@@ -1,383 +1,383 @@
-function AudioSynthView() {
+function AudioSynthView(score) {
 
-	var isMobile = !!navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
-	if(isMobile) { var evtListener = ['touchstart', 'touchend']; } else { var evtListener = ['mousedown', 'mouseup']; }
+  var isMobile = !!navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
+  if(isMobile) { var evtListener = ['touchstart', 'touchend']; } else { var evtListener = ['mousedown', 'mouseup']; }
 
-	var __audioSynth = new AudioSynth();
-	__audioSynth.setVolume(0.5);
-	var __octave = 4;
+  var __audioSynth = new AudioSynth();
+  __audioSynth.setVolume(0.5);
+  var __octave = 4;
 
-	// Change octave
-	// var fnChangeOctave = function(x) {
+  // Key bindings, notes to keyCodes.
+  var keyboard = {
 
-	// 	x |= 0;
+    /* 2 */
+    50: 'C#,-1',
 
-	// 	__octave += x;
+    /* 3 */
+    51: 'D#,-1',
 
-	// 	__octave = Math.min(5, Math.max(3, __octave));
+    /* 5 */
+    53: 'F#,-1',
 
-	//   var octaveName = document.getElementsByName('OCTAVE_LABEL');
-	// 	var i = octaveName.length;
-	// 	while(i--) {
-	// 		var val = parseInt(octaveName[i].getAttribute('value'));
-	// 		octaveName[i].innerHTML = (val + __octave);
-	// 	}
+    /* 6 */
+    54: 'G#,-1',
 
-	// 	document.getElementById('OCTAVE_LOWER').innerHTML = __octave-1;
-	// 	document.getElementById('OCTAVE_UPPER').innerHTML = __octave+1;
+    /* 7 */
+    55: 'A#,-1',
 
-	// };
+    /* 9 */
+    57: 'C#,0',
 
-	// Key bindings, notes to keyCodes.
-	var keyboard = {
+    /* 0 */
+    48: 'D#,0',
 
-			/* 2 */
-			50: 'C#,-1',
+    /* + */
+    187: 'F#,0',
+    61: 'F#,0',
 
-			/* 3 */
-			51: 'D#,-1',
+    /* Q */
+    81: 'C,-1',
 
-			/* 5 */
-			53: 'F#,-1',
+    /* W */
+    87: 'D,-1',
 
-			/* 6 */
-			54: 'G#,-1',
+    /* E */
+    69: 'E,-1',
 
-			/* 7 */
-			55: 'A#,-1',
+    /* R */
+    82: 'F,-1',
 
-			/* 9 */
-			57: 'C#,0',
+    /* T */
+    84: 'G,-1',
 
-			/* 0 */
-			48: 'D#,0',
+    /* Y */
+    89: 'A,-1',
 
-			/* + */
-			187: 'F#,0',
-			61: 'F#,0',
+    /* U */
+    85: 'B,-1',
 
-			/* Q */
-			81: 'C,-1',
+    /* I */
+    73: 'C,0',
 
-			/* W */
-			87: 'D,-1',
+    /* O */
+    79: 'D,0',
 
-			/* E */
-			69: 'E,-1',
+    /* P */
+    80: 'E,0',
 
-			/* R */
-			82: 'F,-1',
+    /* [ */
+    219: 'F,0',
 
-			/* T */
-			84: 'G,-1',
+    /* ] */
+    221: 'G,0',
 
-			/* Y */
-			89: 'A,-1',
+    /* A */
+    65: 'G#,0',
 
-			/* U */
-			85: 'B,-1',
+    /* S */
+    83: 'A#,0',
 
-			/* I */
-			73: 'C,0',
+    /* F */
+    70: 'C#,1',
 
-			/* O */
-			79: 'D,0',
+    /* G */
+    71: 'D#,1',
 
-			/* P */
-			80: 'E,0',
+    /* J */
+    74: 'F#,1',
 
-			/* [ */
-			219: 'F,0',
+    /* K */
+    75: 'G#,1',
 
-			/* ] */
-			221: 'G,0',
+    /* L */
+    76: 'A#,1',
 
-			/* A */
-			65: 'G#,0',
+    /* Z */
+    90: 'A,0',
 
-			/* S */
-			83: 'A#,0',
+    /* X */
+    88: 'B,0',
 
-			/* F */
-			70: 'C#,1',
+    /* C */
+    67: 'C,1',
 
-			/* G */
-			71: 'D#,1',
+    /* V */
+    86: 'D,1',
 
-			/* J */
-			74: 'F#,1',
+    /* B */
+    66: 'E,1',
 
-			/* K */
-			75: 'G#,1',
+    /* N */
+    78: 'F,1',
 
-			/* L */
-			76: 'A#,1',
+    /* M */
+    77: 'G,1',
 
-			/* Z */
-			90: 'A,0',
+    /* , */
+    188: 'A,1',
 
-			/* X */
-			88: 'B,0',
+    /* . */
+    190: 'B,1'
 
-			/* C */
-			67: 'C,1',
+  };
 
-			/* V */
-			86: 'D,1',
+  var reverseLookupText = {};
+  var reverseLookup = {};
 
-			/* B */
-			66: 'E,1',
+  // Create a reverse lookup table.
+  for(var i in keyboard) {
 
-			/* N */
-			78: 'F,1',
+    var val;
 
-			/* M */
-			77: 'G,1',
+    switch(i|0) {
 
-			/* , */
-			188: 'A,1',
+      case 187:
+	val = 61;
+	break;
 
-			/* . */
-			190: 'B,1'
+      case 219:
+	val = 91;
+	break;
 
-		};
+      case 221:
+	val = 93;
+	break;
 
-	var reverseLookupText = {};
-	var reverseLookup = {};
+      case 188:
+	val = 44;
+	break;
 
-	// Create a reverse lookup table.
-	for(var i in keyboard) {
+      case 190:
+	val = 46;
+	break;
 
-		var val;
+      default:
+	val = i;
+	break;
 
-		switch(i|0) {
+    }
 
-			case 187:
-				val = 61;
-				break;
+    reverseLookupText[keyboard[i]] = val;
+    reverseLookup[keyboard[i]] = i;
 
-			case 219:
-				val = 91;
-				break;
+  }
 
-			case 221:
-				val = 93;
-				break;
+  // Keys you have pressed down.
+  var keysPressed = [];
+  var visualKeyboard = null;
+  var selectSound = null;
 
-			case 188:
-				val = 44;
-				break;
+  var fnCreateKeyboard = function(keyboardElement) {
+    // Generate keyboard
+    // This is our main keyboard element! It's populated dynamically based on what you've set above.
+    visualKeyboard = document.getElementById('keyboard');
+    selectSound = document.getElementById('sound');
 
-			case 190:
-				val = 46;
-				break;
+    var iKeys = 0;
+    var iWhite = 0;
+    var notes = __audioSynth._notes;
 
-			default:
-				val = i;
-				break;
-
-		}
-
-		reverseLookupText[keyboard[i]] = val;
-		reverseLookup[keyboard[i]] = i;
-
+    for(var i=-1;i<=1;i++) {
+      for(var n in notes) {
+	if(n[2]!='b') {
+	  var thisKey = document.createElement('div');
+	  if(n.length>1) {
+	    thisKey.className = 'black key';
+	    thisKey.style.width = '30px';
+	    thisKey.style.height = '40%'; // '120px';
+	    thisKey.style.left = (40 * (iWhite - 1)) + 25 + 'px';
+	  } else {
+	    thisKey.className = 'white key';
+	    thisKey.style.width = '40px';
+	    thisKey.style.height = '100%'; // '200px';
+	    thisKey.style.left = 40 * iWhite + 'px';
+	    iWhite++;
+	  }
+	  // var label = document.createElement('div');
+	  // label.className = 'label';
+	  // label.innerHTML = '<b>' + String.fromCharCode(reverseLookupText[n + ',' + i]) + '</b>' + '<br /><br />' + n.substr(0,1) +
+	  // 	'<span name="OCTAVE_LABEL" value="' + i + '">' + (__octave + parseInt(i)) + '</span>' + (n.substr(1,1)?n.substr(1,1):'');
+	  // thisKey.appendChild(label);
+	  thisKey.setAttribute('ID', 'KEY_' + n + ',' + i);
+	  thisKey.addEventListener(evtListener[0], (function(_temp) { return function() { fnPlayKeyboard({keyCode:_temp}); } })(reverseLookup[n + ',' + i]));
+	  visualKeyboard[n + ',' + i] = thisKey;
+	  visualKeyboard.appendChild(thisKey);
+	  iKeys++;
 	}
+      }
+    }
 
-	// Keys you have pressed down.
-	var keysPressed = [];
-	var visualKeyboard = null;
-	var selectSound = null;
+    visualKeyboard.style.width = iWhite * 40 + 'px';
 
-	var fnCreateKeyboard = function(keyboardElement) {
-		// Generate keyboard
-		// This is our main keyboard element! It's populated dynamically based on what you've set above.
-		visualKeyboard = document.getElementById('keyboard');
-		selectSound = document.getElementById('sound');
+    window.addEventListener(evtListener[1], function() { n = keysPressed.length; while(n--) { fnRemoveKeyBinding({keyCode:keysPressed[n]}); } });
 
-		var iKeys = 0;
-		var iWhite = 0;
-		var notes = __audioSynth._notes;
+  };
 
-		for(var i=-1;i<=1;i++) {
-			for(var n in notes) {
-				if(n[2]!='b') {
-					var thisKey = document.createElement('div');
-					if(n.length>1) {
-						thisKey.className = 'black key';
-						thisKey.style.width = '30px';
-					        thisKey.style.height = '60%'; // '120px';
-						thisKey.style.left = (40 * (iWhite - 1)) + 25 + 'px';
-					} else {
-						thisKey.className = 'white key';
-						thisKey.style.width = '40px';
-					        thisKey.style.height = '100%'; // '200px';
-						thisKey.style.left = 40 * iWhite + 'px';
-						iWhite++;
-					}
-					// var label = document.createElement('div');
-					// label.className = 'label';
-					// label.innerHTML = '<b>' + String.fromCharCode(reverseLookupText[n + ',' + i]) + '</b>' + '<br /><br />' + n.substr(0,1) +
-					// 	'<span name="OCTAVE_LABEL" value="' + i + '">' + (__octave + parseInt(i)) + '</span>' + (n.substr(1,1)?n.substr(1,1):'');
-					// thisKey.appendChild(label);
-					thisKey.setAttribute('ID', 'KEY_' + n + ',' + i);
-					thisKey.addEventListener(evtListener[0], (function(_temp) { return function() { fnPlayKeyboard({keyCode:_temp}); } })(reverseLookup[n + ',' + i]));
-					visualKeyboard[n + ',' + i] = thisKey;
-					visualKeyboard.appendChild(thisKey);
-					iKeys++;
-				}
-			}
-		}
+  // Creates our audio player
+  var fnPlayNote = function(note, octave) {
 
-		visualKeyboard.style.width = iWhite * 40 + 'px';
+    src = __audioSynth.generate(selectSound.value, note, octave, 2);
+    container = new Audio(src);
+    container.addEventListener('ended', function() { container = null; });
+    container.addEventListener('loadeddata', function(e) { e.target.play(); });
+    container.autoplay = false;
+    container.setAttribute('type', 'audio/wav');
+    /*document.body.appendChild(container);*/
+    container.load();
+    return container;
 
-		window.addEventListener(evtListener[1], function() { n = keysPressed.length; while(n--) { fnRemoveKeyBinding({keyCode:keysPressed[n]}); } });
+  };
 
-	};
+  // Detect keypresses, play notes.
 
-	// Creates our audio player
-	var fnPlayNote = function(note, octave) {
+  var fnPlayKeyboard = function(e) {
 
-		src = __audioSynth.generate(selectSound.value, note, octave, 2);
-		container = new Audio(src);
-		container.addEventListener('ended', function() { container = null; });
-		container.addEventListener('loadeddata', function(e) { e.target.play(); });
-		container.autoplay = false;
-		container.setAttribute('type', 'audio/wav');
-		/*document.body.appendChild(container);*/
-		container.load();
-		return container;
+    var i = keysPressed.length;
+    while(i--) {
+      if(keysPressed[i]==e.keyCode) {
+	return false;
+      }
+    }
+    keysPressed.push(e.keyCode);
 
-	};
+    switch(e.keyCode) {
+	// space
+      case 16:
+	break;
+	fnPlaySong([
+	  ['E,0', 8],
+	  ['D,0', 8],
+	  ['C,0', 2],
+	  ['C,0', 8],
+	  ['D,0', 8],
+	  ['C,0', 8],
+	  ['E,0', 8],
+	  ['D,0', 1],
+	  ['C,0', 8],
+	  ['D,0', 8],
+	  ['E,0', 2],
+	  ['A,0', 8],
+	  ['G,0', 8],
+	  ['E,0', 8],
+	  ['C,0', 8],
+	  ['D,0', 1],
+	  ['A,0', 8],
+	  ['B,0', 8],
+	  ['C,1', 2],
+	  ['B,0', 8],
+	  ['C,1', 8],
+	  ['D,1', 8],
+	  ['C,1', 8],
+	  ['A,0', 1],
+	  ['G,0', 8],
+	  ['A,0', 8],
+	  ['B,0', 2],
+	  ['C,1', 8],
+	  ['B,0', 8],
+	  ['A,0', 8],
+	  ['G,0', 8],
+	  ['A,0', 1]
+	]);
+	break;
 
-	// Detect keypresses, play notes.
+    }
 
-	var fnPlayKeyboard = function(e) {
+    if(keyboard[e.keyCode]) {
+      if(visualKeyboard[keyboard[e.keyCode]]) {
+	visualKeyboard[keyboard[e.keyCode]].style.backgroundColor = '#ff0000';
+	visualKeyboard[keyboard[e.keyCode]].style.marginTop = '5px';
+	visualKeyboard[keyboard[e.keyCode]].style.boxShadow = 'none';
+      }
+      var arrPlayNote = keyboard[e.keyCode].split(',');
+      var note = arrPlayNote[0];
+      var octaveModifier = arrPlayNote[1]|0;
+      fnPlayNote(note, __octave + octaveModifier);
+    } else {
+      return false;
+    }
 
-		var i = keysPressed.length;
-		while(i--) {
-			if(keysPressed[i]==e.keyCode) {
-				return false;
-			}
-		}
-		keysPressed.push(e.keyCode);
+  }
 
-		switch(e.keyCode) {
+  // Remove key bindings once note is done.
 
-			// // left
-			// case 37:
-			// 	fnChangeOctave(-1);
-			// 	break;
+  var fnRemoveKeyBinding = function(e) {
 
-			// // right
-			// case 39:
-			// 	fnChangeOctave(1);
-			// 	break;
-
-			// space
-			case 16:
-				break;
-				fnPlaySong([
-					['E,0', 8],
-					['D,0', 8],
-					['C,0', 2],
-					['C,0', 8],
-					['D,0', 8],
-					['C,0', 8],
-					['E,0', 8],
-					['D,0', 1],
-					['C,0', 8],
-					['D,0', 8],
-					['E,0', 2],
-					['A,0', 8],
-					['G,0', 8],
-					['E,0', 8],
-					['C,0', 8],
-					['D,0', 1],
-					['A,0', 8],
-					['B,0', 8],
-					['C,1', 2],
-					['B,0', 8],
-					['C,1', 8],
-					['D,1', 8],
-					['C,1', 8],
-					['A,0', 1],
-					['G,0', 8],
-					['A,0', 8],
-					['B,0', 2],
-					['C,1', 8],
-					['B,0', 8],
-					['A,0', 8],
-					['G,0', 8],
-					['A,0', 1]
-				]);
-				break;
-
-		}
-
-		if(keyboard[e.keyCode]) {
-			if(visualKeyboard[keyboard[e.keyCode]]) {
-				visualKeyboard[keyboard[e.keyCode]].style.backgroundColor = '#ff0000';
-				visualKeyboard[keyboard[e.keyCode]].style.marginTop = '5px';
-				visualKeyboard[keyboard[e.keyCode]].style.boxShadow = 'none';
-			}
-			var arrPlayNote = keyboard[e.keyCode].split(',');
-			var note = arrPlayNote[0];
-			var octaveModifier = arrPlayNote[1]|0;
-			fnPlayNote(note, __octave + octaveModifier);
-		} else {
-			return false;
-		}
-
+    var i = keysPressed.length;
+    while(i--) {
+      if(keysPressed[i]==e.keyCode) {
+	if(visualKeyboard[keyboard[e.keyCode]]) {
+	  visualKeyboard[keyboard[e.keyCode]].style.backgroundColor = '';
+	  visualKeyboard[keyboard[e.keyCode]].style.marginTop = '';
+	  visualKeyboard[keyboard[e.keyCode]].style.boxShadow = '';
 	}
+	keysPressed.splice(i, 1);
+      }
+    }
 
-	// Remove key bindings once note is done.
+  }
 
-	var fnRemoveKeyBinding = function(e) {
+  var fnPlaySong = function(arr) {
 
-		var i = keysPressed.length;
-		while(i--) {
-			if(keysPressed[i]==e.keyCode) {
-				if(visualKeyboard[keyboard[e.keyCode]]) {
-					visualKeyboard[keyboard[e.keyCode]].style.backgroundColor = '';
-					visualKeyboard[keyboard[e.keyCode]].style.marginTop = '';
-					visualKeyboard[keyboard[e.keyCode]].style.boxShadow = '';
-				}
-				keysPressed.splice(i, 1);
-			}
-		}
+    if(arr.length>0) {
 
-	}
+      var noteLen = 1000*(1/parseInt(arr[0][1]));
+      if(!(arr[0][0] instanceof Array)) {
+	arr[0][0] = [arr[0][0]];
+      }
+      var i = arr[0][0].length;
+      var keys = [];
+      while(i--) {
+	keys.unshift(reverseLookup[arr[0][0][i]]);
+	fnPlayKeyboard({keyCode:keys[0]});
+      }
+      arr.shift();
+      setTimeout(function(array, val){ return function() { var i = val.length; while(i--) { fnRemoveKeyBinding({keyCode:val[i]}); } fnPlaySong(array); } }(arr, keys), noteLen);
 
-	var fnPlaySong = function(arr) {
+    }
 
-		if(arr.length>0) {
+  };
 
-			var noteLen = 1000*(1/parseInt(arr[0][1]));
-			if(!(arr[0][0] instanceof Array)) {
-				arr[0][0] = [arr[0][0]];
-			}
-			var i = arr[0][0].length;
-			var keys = [];
-			while(i--) {
-				keys.unshift(reverseLookup[arr[0][0][i]]);
-				fnPlayKeyboard({keyCode:keys[0]});
-			}
-			arr.shift();
-			setTimeout(function(array, val){ return function() { var i = val.length; while(i--) { fnRemoveKeyBinding({keyCode:val[i]}); } fnPlaySong(array); } }(arr, keys), noteLen);
-
-		}
-
-	};
-
-	// Set up global event listeners
-
-	window.addEventListener('keydown', fnPlayKeyboard);
-	window.addEventListener('keyup', fnRemoveKeyBinding);
-	// document.getElementById('-_OCTAVE').addEventListener('click', function() { fnChangeOctave(-1); });
-	// document.getElementById('+_OCTAVE').addEventListener('click', function() { fnChangeOctave(1); });
-
-	Object.defineProperty(this, 'draw', {
-		value: fnCreateKeyboard
-	});
+  // Set up global event listeners
+  document.getElementById('testbutton').onclick = () => {
+    fnPlaySong([
+      ['E,0', 8],
+      ['D,0', 8],
+      ['C,0', 2],
+      ['C,0', 8],
+      ['D,0', 8],
+      ['C,0', 8],
+      ['E,0', 8],
+      ['D,0', 1],
+      ['C,0', 8],
+      ['D,0', 8],
+      ['E,0', 2],
+      ['A,0', 8],
+      ['G,0', 8],
+      ['E,0', 8],
+      ['C,0', 8],
+      ['D,0', 1],
+      ['A,0', 8],
+      ['B,0', 8],
+      ['C,1', 2],
+      ['B,0', 8],
+      ['C,1', 8],
+      ['D,1', 8],
+      ['C,1', 8],
+      ['A,0', 1],
+      ['G,0', 8],
+      ['A,0', 8],
+      ['B,0', 2],
+      ['C,1', 8],
+      ['B,0', 8],
+      ['A,0', 8],
+      ['G,0', 8],
+      ['A,0', 1]
+    ]);
+  };
+  window.addEventListener('keydown', fnPlayKeyboard);
+  window.addEventListener('keyup', fnRemoveKeyBinding);
+  Object.defineProperty(this, 'draw', {
+    value: fnCreateKeyboard
+  });
 
 }
