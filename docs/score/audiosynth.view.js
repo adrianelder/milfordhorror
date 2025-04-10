d8d3b0ff89279a7c5d3c5ffd8d97ae968aaed49c
Adrian Elder <adrian.elder@gmail.com>
2025-04-10 10:48:39 -0400

    score work

--- docs/score/audiosynth.view.js
+++ docs/score/audiosynth.view.js
@@ -1,3 +1,81 @@
+// CONTEXT AND MASTER VOLUME
+var AudioContext = window.AudioContext ||
+    window.webkitAudioContext;
+  
+const context = new AudioContext();
+const masterVolume = context.createGain();
+masterVolume.connect(context.destination);
+masterVolume.gain.value = 0.2
+const delay = context.createDelay();
+const feedback = context.createGain();
+const delayAmountGain = context.createGain();
+
+delayAmountGain.connect(delay)
+delay.connect(feedback)
+feedback.connect(delay)
+delay.connect(masterVolume)
+
+
+delay.delayTime.value = 0;
+delayAmountGain.gain.value = 0;
+feedback.gain.value = 0;
+
+const notes = {
+'C':[16.35,32.7,65.41,130.81,261.63,523.25,1046.5,2093,4186],
+'C#':[17.32,34.65,69.3,138.59,277.18,554.37,1108.73,2217.46,4434.92],
+'D':[18.35,36.71,73.42,146.83,293.66,587.33,1174.66,2349.32,4698.63],
+'D#':[19.45,38.89,77.78,155.56,311.13,622.25,1244.51,2489,4978],
+'E':[20.6,41.2,82.41,164.81,329.63,659.25,1318.51,2637,5274],
+'F':[21.83,43.65,87.31,174.61,349.23,698.46,1396.91,2793.83,5587.65],
+'F#':[23.12,46.25,92.5,185,369.99,739.99,1479.98,2959.96,5919.91],
+'G':[24.5,49,98,196,392,783.99,1567.98,3135.96,6271.93],
+'G#':[25.96,51.91,103.83,207.65,415.3,830.61,1661.22,3322.44,6644.88],
+'A':[27.5,55,110,220,440,880,1760,3520,7040],
+'A#':[29.14,58.27,116.54,233.08,466.16,932.33,1864.66,3729.31,7458.62],
+'B':[30.87,61.74,123.47,246.94,493.88,987.77,1975.53,3951,7902.13],
+}
+
+function playNote(note, octave, waveform) {
+    // Envelope
+    let attackTime = 0.3;
+    let sustainLevel = 0.8;
+    let releaseTime = 0.3;
+    let noteLength = 0.7;
+    let vibratoSpeed = 10;
+    let vibratoAmount = 0;
+    
+    const osc = context.createOscillator();
+    const noteGain = context.createGain();
+    noteGain.gain.setValueAtTime(0, 0);
+    noteGain.gain.linearRampToValueAtTime(sustainLevel, context.currentTime + noteLength * attackTime);
+    noteGain.gain.setValueAtTime(sustainLevel, context.currentTime + noteLength - noteLength * releaseTime);
+    noteGain.gain.linearRampToValueAtTime(0, context.currentTime + noteLength);
+
+    lfoGain = context.createGain();
+    lfoGain.gain.setValueAtTime(vibratoAmount, 0);
+    lfoGain.connect(osc.frequency)
+
+    lfo = context.createOscillator();
+    lfo.frequency.setValueAtTime(vibratoSpeed, 0);
+    lfo.start(0);
+    lfo.stop(context.currentTime + noteLength);
+    lfo.connect(lfoGain); 
+
+
+    osc.type = waveform;
+    osc.frequency.setValueAtTime(notes[note][octave - 1], 0);
+    osc.start(0);
+    osc.stop(context.currentTime + noteLength);
+    osc.connect(noteGain);
+
+    noteGain.connect(masterVolume);
+    noteGain.connect(delay);
+}
+
+// document.querySelector('#start').onclick = function (e) {
+//     setInterval(() => playNote('C#', 4), 1000);
+// }
+
 function AudioSynthView(score) {
   let startTime = null;
   let playingSong = false;
@@ -224,15 +302,15 @@ function AudioSynthView(score) {
 
   // Creates our audio player
   var fnPlayNote = function(note, octave) {
-
-    src = __audioSynth.generate(selectSound.value, note, octave, 2);
-    container = new Audio(src);
-    container.addEventListener('ended', function() { container = null; });
-    container.addEventListener('loadeddata', function(e) { e.target.play(); });
-    container.autoplay = false;
-    container.setAttribute('type', 'audio/wav');
-    container.load();
-    return container;
+      playNote(note, octave, selectSound.value);
+    //   src = __audioSynth.generate(selectSound.value, note, octave, 2);
+    // container = new Audio(src);
+    // container.addEventListener('ended', function() { container = null; });
+    // container.addEventListener('loadeddata', function(e) { e.target.play(); });
+    // container.autoplay = false;
+    // container.setAttribute('type', 'audio/wav');
+    // container.load();
+    // return container;
 
   };
 
